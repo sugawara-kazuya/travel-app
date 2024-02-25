e58132b13cffc63deff5db8626ca937915b4abd2
sugawara-kazuya <kz515yssg@gmail.com>
2024-02-25 04:21:35 +0000

    環境構築

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "src": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
